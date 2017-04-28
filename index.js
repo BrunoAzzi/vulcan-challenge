8bc07ca0860e24642363259fc1cd397ced8962c5
Bruno Azzi <bruno.azzi@icloud.com>
2017-04-28 03:45:57 -0300

    Random shit to heroku

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
